da20c85257d6abed0c96750dd3df3190fca12e27
Rahulkannan S <rahulknns@gmail.com>
2024-03-01 22:17:59 +0530

    define thruster class blueprint

---
+++ include/pins_info.hpp
@@ -0,0 +1,6 @@
+#ifndef SENSIING_AND_CONTROL_UNIT_INCLUDE_PINS_INFO_HPP
+#define SENSIING_AND_CONTROL_UNIT_INCLUDE_PINS_INFO_HPP
+#include <thruster_info.hpp>
+const int kThrusterPins[kThrusterCount] = {2, 3, 4, 5, 6, 7};
+const int kLumenPin = 8;
+#endif // SENSIING_AND_CONTROL_UNIT_INCLUDE_PINS_INFO_HPP
--- src/thruster/thruster.hpp
+++ src/thruster/thruster.hpp
@@ -1,4 +1,20 @@
 #ifndef SENSING_AND_CONTROL_UNIT_SRC_THRUSTER_THRUSTER_HPP
 #define SENSING_AND_CONTROL_UNIT_SRC_THRUSTER_THRUSTER_HPP
+#include "thruster_info.hpp"
+#include <Servo.h>
+namespace sensing_and_control_unit
+{
+    class Thrusters
+    {
+    private:
+        Servo thrusters[kThrusterCount];
+    public:
+        Thrusters();
+        ~Thrusters();
+        void initialize();
+        void setPWMs(int pwm_values[kThrusterCount]);
+    };
+} // namespace sensing_and_control_unit
+
 
 #endif // SENSING_AND_CONTROL_UNIT_SRC_THRUSTER_THRUSTER_HPP
