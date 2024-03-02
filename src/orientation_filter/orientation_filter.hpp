bd21ec9b3bd48845ad8ee9dbed71b2df7c9e06e3
Karthick <karthickkp2005@gmail.com>
2024-03-02 17:43:31 +0530

    Added Fusion library in lib

--- src/orientation_filter/orientation_filter.hpp
+++ src/orientation_filter/orientation_filter.hpp
@@ -1,7 +1,9 @@
 #ifndef ORIENTATION_FILTER_INCLUDE_ORIENTATION_FILTER_HPP
 #define ORIENTATION_FILTER_INCLUDE_ORIENTATION_FILTER_HPP
-#include <vec3.hpp>
 #include <Arduino.h>
+
+#include <vec3.hpp>
+
 #include "Fusion.h"
 
 namespace sensing_and_control_unit {
@@ -12,15 +14,14 @@ class OrientationFilter {
   FusionAhrs ahrs;
   FusionEuler euler;
   bool start = true;
-  double current_time=millis();
-  double prev_time=current_time;
-  double delta_time=0;
-
+  double current_time = millis();
+  double prev_time = current_time;
+  double delta_time = 0;
 
  public:
   OrientationFilter(/* args */);
   ~OrientationFilter();
-  
+
   Vec3f orientation;
   void initialize(unsigned int);
 
