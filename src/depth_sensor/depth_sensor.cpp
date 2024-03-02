e136e496183f2c0b7f2535b784f0c1f368e6c65f
Karthick <karthickkp2005@gmail.com>
2024-03-02 19:51:55 +0530

    Modified depth_Sensor as per style guide

--- src/depth_sensor/depth_sensor.cpp
+++ src/depth_sensor/depth_sensor.cpp
@@ -5,23 +5,21 @@ DepthSensor::DepthSensor() {}
 DepthSensor::~DepthSensor() {}
 
 void DepthSensor::initialize(Communication& communication) {
-    depth_sensor_offset = communication.depthSensorOffset;
-    depth_sensor_status= depthSensor.init();
-    depthSensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
+  depth_sensor_offset_ = communication.getDepthSensorOffset();
+  depth_sensor_status_ = depth_sensor_.init();
+  depth_sensor_.setFluidDensity(997);  // kg/m^3 (freshwater, 1029 for seawater)
 }
 
 void DepthSensor::update() {
-    depthSensor.read();
-    raw_depth=depthSensor.depth();
+  depth_sensor_.read();
+  raw_depth_ = depth_sensor_.depth();
 }
 
-int DepthSensor::getRawReading() { 
-    return raw_depth;
-}
+int DepthSensor::getRawReading() { return raw_depth_; }
 
-float DepthSensor::getDepth() { 
-    depth = raw_depth - depth_sensor_offset;
-    return depth;
+float DepthSensor::getDepth() {
+  depth_ = raw_depth_ - depth_sensor_offset_;
+  return depth_;
 }
 
 }  // namespace sensing_and_control_unit
--- src/depth_sensor/depth_sensor.hpp
+++ src/depth_sensor/depth_sensor.hpp
@@ -1,21 +1,21 @@
 #ifndef SENSING_AND_CONTROL_UNIT_SRC_DEPTH_SENSOR_DEPTH_SENSOR_HPP
 #define SENSING_AND_CONTROL_UNIT_SRC_DEPTH_SENSOR_DEPTH_SENSOR_HPP
-#include "communication/communication.hpp"
 #include "MS5837.h"
-
-
+#include "communication/communication.hpp"
 
 namespace sensing_and_control_unit {
 class DepthSensor {
  private:
   /* data */
-  MS5837 depthSensor;
-  float depth_sensor_offset;
-  bool depth_sensor_status;
+  MS5837 depth_sensor_;
+  float raw_depth_;
+  float depth_;
+  float depth_sensor_offset_;
+  bool depth_sensor_status_;
+
  public:
   DepthSensor(/* args */);
   ~DepthSensor();
-  float raw_depth,depth;
   void initialize(Communication& communication);
   void update();
   int getRawReading();
