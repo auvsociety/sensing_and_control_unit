08a7b010f60530f8d683b1f0636b7cc9838f5d4e
Srinidhi <srinidhip1234@gmail.com>
2024-03-02 19:15:43 +0530

    lumen_interface definitions updated - INITIAL COMMIT

--- src/lumen/lumen.cpp
+++ src/lumen/lumen.cpp
@@ -6,13 +6,26 @@ Lumen::~Lumen() {}
 
 void Lumen::initialize() {
   lumen_.attach(kLumenPin);
-  lumen_.write(0);
+  lumen_.writeMicroseconds(kMinBrightness);
 }
 
-void Lumen::setBrightness(int brightness) { lumen_.write(brightness); }
+void Lumen::setBrightness(float brightness) {
+  int32_t pwm_value = map(brightness, 0, 1, kMinBrightness, kMaxBrightness);
+  pwm_value = limitToMinMax(pwm_value, kMinBrightness, kMaxBrightness);
+  lumen_.writeMicroseconds(pwm_value);
+}
 
-void Lumen::turnOn() { lumen_.write(180); }
+void Lumen::turnOn() { lumen_.writeMicroseconds(kMaxBrightness); }
 
-void Lumen::turnOff() { lumen_.write(0); }
+void Lumen::turnOff() { lumen_.writeMicroseconds(kMinBrightness); }
+
+int32_t Lumen::limitToMinMax(int32_t value, int32_t min, int32_t max) {
+  if (value < min) {
+    value = min;
+  } else if (value > max) {
+    value = max;
+  }
+  return value;
+}
 
 }  // namespace sensing_and_control_unit
