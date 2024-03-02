4d50e10bcb676dd69224601100cc465da76fee9f
Karthick <karthickkp2005@gmail.com>
2024-03-02 19:06:29 +0530

    Named variables as per style guide

--- src/imu/imu.cpp
+++ src/imu/imu.cpp
@@ -5,65 +5,88 @@ Imu::Imu() {}
 Imu::~Imu() {}
 
 void Imu::initialize(Communication& communication) {
-    
-    accelerometerOffsets = communication.accelerometerOffsets;
-    gyroscopeOffsets = communication.gyroscopeOffsets;
-    magnetometerHardIronOffsets = communication.magnetometerHardIronOffsets;
-    magnetometerSoftIronOffsets = communication.magnetometerSoftIronOffsets;
+  accelerometer_magnetometer_ =
+      Adafruit_FXOS8700(accelerometer_address, magnetometer_address);
+  Vec3f accelerometer_offset_vec3_ = communication.getAccelerometerOffset();
+  Vec3f gyroscope_offset_vec3_ = communication.getGyroscopeOffset();
+  Vec3f hard_iron_offset_vec3_ = communication.getMagnetometerHardIronOffset();
+  Mat3x3f soft_iron_matrix_mat3x3_ =
+      communication.getMagnetometerSoftIronMatrix();
 
-    accelerometer_magnetometer_status = accelerometerMagnetometer.begin();
-    gyroscope_status =  gyroscope.begin();
-    gyroscope.setAccelerometerRange(ACCELERO_METER_RANGE_2);
-    gyroscope.setGyroscopeRange(GYROSCOPE_RANGE_250);
-    gyroscope.setSampleRateDivider(0);
-    gyroscope.disableSleepMode();   
+  accelerometer_offset_ = {accelerometer_offset_vec3_.x,
+                           accelerometer_offset_vec3_.y,
+                           accelerometer_offset_vec3_.z};
+  gyroscope_offset_ = {gyroscope_offset_vec3_.x, gyroscope_offset_vec3_.y,
+                       gyroscope_offset_vec3_.z};
+  hard_iron_offset_ = {hard_iron_offset_vec3_.x, hard_iron_offset_vec3_.y,
+                       hard_iron_offset_vec3_.z};
+  soft_iron_matrix_ = {
+      soft_iron_matrix_mat3x3_.m00, soft_iron_matrix_mat3x3_.m01,
+      soft_iron_matrix_mat3x3_.m02, soft_iron_matrix_mat3x3_.m10,
+      soft_iron_matrix_mat3x3_.m11, soft_iron_matrix_mat3x3_.m12,
+      soft_iron_matrix_mat3x3_.m20, soft_iron_matrix_mat3x3_.m21,
+      soft_iron_matrix_mat3x3_.m22};
 
+  accelerometer_magnetometer_status_ = accelerometer_magnetometer_.begin();
+  gyroscope_status_ = gyroscope_.begin();
+  gyroscope_.setAccelerometerRange(ACCELERO_METER_RANGE_2);
+  gyroscope_.setGyroscopeRange(GYROSCOPE_RANGE_250);
+  gyroscope_.setSampleRateDivider(0);
+  gyroscope_.disableSleepMode();
 }
 
 void Imu::update() {
-    sensors_event_t accel_event,mag_event;
-    accelerometerMagnetometer.getEvent(&accel_event,&mag_event);
+  sensors_event_t accel_event, mag_event;
+  accelerometer_magnetometer_.getEvent(&accel_event, &mag_event);
 
-    gyroscope.getSensorsReadings(raw_accelerometer_readings.y, raw_accelerometer_readings.x, raw_accelerometer_readings.z, raw_gyroscope_readings.y, raw_gyroscope_readings.x, raw_gyroscope_readings.z);
-    raw_accelerometer_readings.y = -raw_accelerometer_readings.y;   // Data changed to match the orientation of both sensors in marty)
-    raw_gyroscope_readings.y = -raw_accelerometer_readings.y;
-    
-    raw_magnetometer_readings = {mag_event.magnetic.x, mag_event.magnetic.y, mag_event.magnetic.z};
-    
+  gyroscope_.getSensorsReadings(
+      raw_accelerometer_reading_.y, raw_accelerometer_reading_.x,
+      raw_accelerometer_reading_.z, raw_gyroscope_reading_.y,
+      raw_gyroscope_reading_.x, raw_gyroscope_reading_.z);
+  raw_accelerometer_reading_.y =
+      -raw_accelerometer_reading_.y;  // Data changed to match the orientation
+                                      // of both sensors in marty)
+  raw_gyroscope_reading_.y = -raw_accelerometer_reading_.y;
+
+  raw_magnetometer_reading_ = {mag_event.magnetic.x, mag_event.magnetic.y,
+                               mag_event.magnetic.z};
 }
 
-Vec3f Imu::getRawAccelerometerReading() { 
-    return raw_accelerometer_readings; 
+Vec3f Imu::getRawAccelerometerReading() { return raw_accelerometer_reading_; }
+
+Vec3f Imu::getRawGyroscopeReading() { return raw_gyroscope_reading_; }
+
+Vec3f Imu::getRawMagnetometerReading() { return raw_magnetometer_reading_; }
+
+Vec3f Imu::getAcceleration() {
+  FusionVector accelerometer = {raw_accelerometer_reading_.x,
+                                raw_accelerometer_reading_.y,
+                                raw_accelerometer_reading_.z};
+  accelerometer = FusionCalibrationInertial(
+      accelerometer, accelerometer_misalignment_, accelerometer_sensitivity_,
+      accelerometer_offset_);
+  acceleration_ = {accelerometer.axis.x, accelerometer.axis.y,
+                   accelerometer.axis.z};
+  return acceleration_;
 }
 
-Vec3f Imu::getRawGyroscopeReading() { 
-    return raw_gyroscope_readings;
+Vec3f Imu::getAngularVelocity() {
+  FusionVector gyroscope = {raw_gyroscope_reading_.x, raw_gyroscope_reading_.y,
+                            raw_gyroscope_reading_.z};
+  gyroscope =
+      FusionCalibrationInertial(gyroscope, gyroscope_misalignment_,
+                                gyroscope_sensitivity_, gyroscope_offset_);
+  angular_velocity_ = {gyroscope.axis.x, gyroscope.axis.y, gyroscope.axis.z};
+  return angular_velocity_;
 }
 
-Vec3f Imu::getRawMagnetometerReading() {
-    return raw_magnetometer_readings; 
-}
-
-Vec3f Imu::getAcceleration() { 
-    FusionVector accelerometer = {raw_accelerometer_readings.x, raw_accelerometer_readings.y, raw_accelerometer_readings.z};
-    accelerometer = FusionCalibrationInertial(accelerometer, accelerometerMisalignment, accelerometerSensitivity, accelerometerOffsets);
-    acceleration = {accelerometer.axis.x, accelerometer.axis.y, accelerometer.axis.z};
-    return acceleration; 
-}
-
-Vec3f Imu::getAngularVelocity() { 
-    FusionVector gyroscope = {raw_gyroscope_readings.x, raw_gyroscope_readings.y, raw_gyroscope_readings.z};
-    gyroscope = FusionCalibrationInertial(gyroscope, gyroscopeMisalignment, gyroscopeSensitivity, gyroscopeOffsets);
-    angular_velocity = {gyroscope.axis.x, gyroscope.axis.y, gyroscope.axis.z};
-    return angular_velocity;
-}
-
-Vec3f Imu::getMagneticField() { 
-    FusionVector magnetometer;
-    magnetometer = FusionCalibrationMagnetic(magnetometer, magnetometerSoftIronOffsets, magnetometerHardIronOffsets);
-    magnetic_field = {magnetometer.axis.x, magnetometer.axis.y, magnetometer.axis.z};
-    return magnetic_field; 
+Vec3f Imu::getMagneticField() {
+  FusionVector magnetometer;
+  magnetometer = FusionCalibrationMagnetic(magnetometer, soft_iron_matrix,
+                                           hard_iron_offset);
+  magnetic_field = {magnetometer.axis.x, magnetometer.axis.y,
+                    magnetometer.axis.z};
+  return magnetic_field;
 }
 
 }  // namespace sensing_and_control_unit
-
--- src/imu/imu.hpp
+++ src/imu/imu.hpp
@@ -1,41 +1,47 @@
 #ifndef SENSING_AND_CONTROL_UNIT_SRC_IMU_IMU_HPP
 #define SENSING_AND_CONTROL_UNIT_SRC_IMU_IMU_HPP
-#include "communication/communication.hpp"
-#include "vec3.hpp"
-#include "sensor_info.hpp"
-#include "mpu6050.hpp"
 #include "Adafruit_FXOS8700.h"
 #include "Fusion.h"
-
-
+#include "communication/communication.hpp"
+#include "mpu6050.hpp"
+#include "sensor_info.hpp"
+#include "vec3.hpp"
 
 namespace sensing_and_control_unit {
 class Imu {
-
  private:
-  /* data */
-  MPU6050 gyroscope;
-  Adafruit_FXOS8700 accelerometerMagnetometer = Adafruit_FXOS8700(accelerometerAddress , magnetometerAddress);
+  MPU6050 gyroscope_;
+  Adafruit_FXOS8700 accelerometer_magnetometer_;
 
-  const FusionMatrix gyroscopeMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
-  const FusionVector gyroscopeSensitivity = {GYROSCOPE_SENSITIVITY_250, GYROSCOPE_SENSITIVITY_250, GYROSCOPE_SENSITIVITY_250};
-  FusionVector gyroscopeOffsets = {0.0f, 0.0f, 0.0f};
-  const FusionMatrix accelerometerMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
-  const FusionVector accelerometerSensitivity = {ACCELERO_METER_SENSITIVITY_2,ACCELERO_METER_SENSITIVITY_2, ACCELERO_METER_SENSITIVITY_2};
-  FusionVector accelerometerOffsets = {0.0f, 0.0f, 0.0f};
-  FusionMatrix magnetometerSoftIronOffsets = {0.971, -0.048, -0.024, -0.048, 0.972, -0.011, -0.024, -0.011, 1.063};
-  FusionVector magnetometerHardIronOffsets = {57.59, 88.22, 74.47};
+  const FusionMatrix gyroscope_misalignment_ = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+                                                0.0f, 0.0f, 0.0f, 1.0f};
+  const FusionVector gyroscope_sensitivity_ = {GYROSCOPE_SENSITIVITY_250,
+                                               GYROSCOPE_SENSITIVITY_250,
+                                               GYROSCOPE_SENSITIVITY_250};
+  FusionVector gyroscope_offset_ = {0.0f, 0.0f, 0.0f};
 
-  bool accelerometer_magnetometer_status;
-  bool gyroscope_status;
+  const FusionMatrix accelerometer_misalignment_ = {
+      1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+  const FusionVector accelerometer_sensitivity_ = {
+      ACCELERO_METER_SENSITIVITY_2, ACCELERO_METER_SENSITIVITY_2,
+      ACCELERO_METER_SENSITIVITY_2};
+  FusionVector accelerometer_offset_ = {0.0f, 0.0f, 0.0f};
+
+  FusionMatrix soft_iron_matrix_ = {0.971,  -0.048, -0.024, -0.048, 0.972,
+                                    -0.011, -0.024, -0.011, 1.063};
+  FusionVector hard_iron_offset_ = {57.59, 88.22, 74.47};
+
+  Vec3f raw_accelerometer_reading_, raw_gyroscope_reading_,
+      raw_magnetometer_reading_;
+
+  Vec3f acceleration_, angular_velocity_, magnetic_field_;
+
+  bool accelerometer_magnetometer_status_;
+  bool gyroscope_status_;
 
  public:
   Imu(/* args */);
   ~Imu();
-
-  Vec3f raw_accelerometer_readings,raw_gyroscope_readings,raw_magnetometer_readings;
-  Vec3f acceleration,angular_velocity,magnetic_field;
-
   void initialize(Communication& communication);
 
   void update();
