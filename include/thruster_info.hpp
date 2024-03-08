11118a0d458ac33f7898f63fa1c4fcfcfe06cacd
Srinidhi <srinidhip1234@gmail.com>
2024-03-08 17:38:22 +0530

    thruster_info updated

--- include/thruster_info.hpp
+++ include/thruster_info.hpp
@@ -1,7 +1,38 @@
 #ifndef SENSIING_AND_CONTROL_UNIT_SRC_THRUSTER_THRUSTER_INFO_HPP
 #define SENSIING_AND_CONTROL_UNIT_SRC_THRUSTER_THRUSTER_INFO_HPP
+
+/**
+ * @file thruster_info.hpp
+ * @brief Defines constants related to thruster configuration.
+ */
+
+/**
+ * @brief Number of thrusters in the system.
+ */
+
 const int kThrusterCount = 7;
+
+/**
+ * @brief Initial PWM value for thrusters.
+ * @details This constant represents the initial PWM value used when configuring
+ * thrusters.
+ */
+
 const int32_t kInitThrusterPwm = 1474;
+
+/**
+ * @brief Minimum allowable PWM value for thrusters.
+ * @details This constant represents the minimum allowable PWM value for
+ * thrusters.
+ */
+
 const int32_t kMinPwmValue = 1100;
+
+/**
+ * @brief Maximum allowable PWM value for thrusters.
+ * @details This constant represents the maximum allowable PWM value for
+ * thrusters.
+ */
+
 const int32_t kMaxPwmValue = 1900;
-#endif // SENSIING_AND_CONTROL_UNIT_SRC_THRUSTER_THRUSTER_INFO_HPP
+#endif  // SENSIING_AND_CONTROL_UNIT_SRC_THRUSTER_THRUSTER_INFO_HPP
