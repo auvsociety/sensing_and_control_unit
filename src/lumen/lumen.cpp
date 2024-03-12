4f2a8398b068e7da12beecd87fc932eb80777571
Srinidhi <srinidhip1234@gmail.com>
2024-03-13 00:24:55 +0530

    definitions updated and documentation added

--- src/lumen/lumen.cpp
+++ src/lumen/lumen.cpp
@@ -1,18 +1,95 @@
+// lumen.hpp
+
 #include "lumen.hpp"
 
 namespace sensing_and_control_unit {
+
+/**
+ * @file lumen.cpp
+ * @brief Implements the methods of the Lumen class for controlling and managing
+ * lumen.
+ *
+ * This file implements the functionality declared in lumen.hpp.
+ */
+
 Lumen::Lumen() {}
 Lumen::~Lumen() {}
 
+/**
+ * @brief Initializes the lumen.
+ *
+ * This function attaches the lumen to its designated pin and sets brightness to
+ * kMinBrightness, preparing it for use.
+ *
+ * @note Ensure that kLumenPin is correctly configured.
+ *
+ * @param None
+ * @return None
+ */
+
 void Lumen::initialize() {
   lumen_.attach(kLumenPin);
-  lumen_.write(0);
+  lumen_.writeMicroseconds(kMinBrightness);
 }
 
-void Lumen::setBrightness(int brightness) { lumen_.write(brightness); }
+/**
+ * @brief Sets the brightness of the lumen.
+ *
+ * This function sets the brightness of the lumen based on the provided value.
+ *
+ * @param brightness The brightness value to be set. Should be between 0.0
+ * and 1.0.
+ * @return None
+ */
 
-void Lumen::turnOn() { lumen_.write(180); }
+void Lumen::setBrightness(float brightness) {
+  int32_t pwm_value = map(brightness, 0, 1, kMinBrightness, kMaxBrightness);
+  pwm_value = limitToMinMax(pwm_value, kMinBrightness, kMaxBrightness);
+  lumen_.writeMicroseconds(pwm_value);
+}
 
-void Lumen::turnOff() { lumen_.write(0); }
+/**
+ * @brief Turns on the lumen.
+ *
+ * This function sets the brightness of the lumen to its maximum value,
+ * turning it on.
+ *
+ * @param None
+ * @return None
+ */
+
+void Lumen::turnOn() { lumen_.writeMicroseconds(kMaxBrightness); }
+
+/**
+ * @brief Turns off the lumen.
+ *
+ * This function sets the brightness of the lumen to its minimum value,
+ * turning it off.
+ *
+ * @param None
+ * @return None
+ */
+
+void Lumen::turnOff() { lumen_.writeMicroseconds(kMinBrightness); }
+
+/**
+ * @brief Limits a value to a specified range.
+ *
+ * This function ensures that the input value is within the specified range.
+ *
+ * @param value The value to be limited.
+ * @param min The minimum allowed value.
+ * @param max The maximum allowed value.
+ * @return The limited value within the specified range.
+ */
+
+int32_t Lumen::limitToMinMax(int32_t value, int32_t min, int32_t max) {
+  if (value < min) {
+    value = min;
+  } else if (value > max) {
+    value = max;
+  }
+  return value;
+}
 
 }  // namespace sensing_and_control_unit
--- src/lumen/lumen.hpp
+++ src/lumen/lumen.hpp
@@ -1,20 +1,105 @@
 #ifndef SENSING_AND_CONTROL_UNIT_SRC_LUMEN_LUMEN_HPP
 #define SENSING_AND_CONTROL_UNIT_SRC_LUMEN_LUMEN_HPP
+#include <Arduino.h>
 #include <Servo.h>
 
 #include "pins_info.hpp"
 
 namespace sensing_and_control_unit {
+
+/**
+ * @file lumen.hpp
+ * @brief Contains the declaration of the Lumen class for controlling and
+ * managing lumen.
+ */
+
+/**
+ * @class Lumen
+ * @brief Controls and manages the lumen.
+ *
+ * This class provides methods for initializing the lumen, setting its
+ * brightness, turning it on, and turning it off.
+ */
+
 class Lumen {
  private:
+  /**
+   * @brief Servo object for controlling lumen brightness.
+   */
+
   Servo lumen_;
 
+  /**
+   * @brief Minimum PWM value for lumen brightness.
+   */
+
+  int32_t kMinBrightness = 1100;
+
+  /**
+   * @brief Maximum PWM value for lumen brightness.
+   */
+
+  int32_t kMaxBrightness = 1900;
+
+  /**
+   * @brief Limits a value to a specified range.
+   *
+   * @param value The value to be limited.
+   * @param min The minimum value of the range.
+   * @param max The maximum value of the range.
+   * @return The limited value within the specified range.
+   */
+
+  int32_t limitToMinMax(int32_t value, int32_t min, int32_t max);
+
  public:
+  /**
+   * @brief Constructor for the Lumen class.
+   *
+   * Initializes the Lumen class by creating a Servo object.
+   */
+
   Lumen(/* args */);
+
+  /**
+   * @brief Destructor for the Lumen class.
+   *
+   * Cleans up resources used by the Lumen class.
+   */
+
   ~Lumen();
+
+  /**
+   * @brief Initializes the lumen.
+   *
+   * This method sets up the Servo object for controlling lumen brightness.
+   */
+
   void initialize();
-  void setBrightness(int brightness);
+
+  /**
+   * @brief Sets the brightness of the lumen.
+   *
+   * @param brightness The brightness value to be set, ranging from 0.0 to 1.0.
+   */
+
+  void setBrightness(float brightness);
+
+  /**
+   * @brief Turns on the lumen.
+   *
+   * This method sets the brightness of the lumen to its maximum value, turning
+   * it on.
+   */
   void turnOn();
+
+  /**
+   * @brief Turns off the lumen.
+   *
+   * This method sets the brightness of the lumen to its minimum value, turning
+   * it off.
+   */
+
   void turnOff();
 };
 }  // namespace sensing_and_control_unit
