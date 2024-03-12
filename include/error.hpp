9edabd11b0ea4ae65ad9e13eb2f48677cec947b4
Srinidhi <srinidhip1234@gmail.com>
2024-03-13 00:47:55 +0530

    included errorlevel under namespace

--- include/error.hpp
+++ include/error.hpp
@@ -1,11 +1,12 @@
 #ifndef SENSING_AND_CONTROL_UNIT_SRC_ERROR_HPP
 #define SENSING_AND_CONTROL_UNIT_SRC_ERROR_HPP
-enum ErrorLevel
-{
-    INFO,
-    WARNING,
-    ERROR,
-    FATAL
-};
+namespace sensing_and_control_unit {
+
+/**
+ * @file error.hpp
+ * @brief Defines error levels for the sensing and control unit.
+ */
+enum class ErrorLevel { INFO, WARNING, ERROR, FATAL };
+}  // namespace sensing_and_control_unit
 typedef enum ErrorLevel ErrorLevel;
-#endif // SENSING_AND_CONTROL_UNIT_SRC_ERROR_HPP
+#endif  // SENSING_AND_CONTROL_UNIT_SRC_ERROR_HPP
