8fb9b2d36bac4606a7590edfbf4f82882db1e081
PavanAdarsh <pavanadarsh2019@gmail.com>
2024-10-28 22:35:31 +0530

    Update orientation_filter.hpp

--- src/orientation_filter/orientation_filter.hpp
+++ src/orientation_filter/orientation_filter.hpp
@@ -1,13 +1,26 @@
 #ifndef ORIENTATION_FILTER_INCLUDE_ORIENTATION_FILTER_HPP
 #define ORIENTATION_FILTER_INCLUDE_ORIENTATION_FILTER_HPP
-#include <vec3.hpp>
-namespace sensing_and_control_unit {
-class OrientationFilter {
+#include "vec3.hpp"
+#include "imu/imu.hpp"
+#include "Fusion/Fusion.h"
+#include <Arduino.h>
+
+namespace sensing_and_control_unit{
+class OrientationFilter: public Imu {
  private:
-  /* data */
+  Vec3f acceleration, angular_velocity, magnetic_field;
+  float ax,ay,az,gx,gy,gz,mx,my,mz;
+  long int current_time=0, elapsed_time=0, delta_time=0;
+  int sample_rate = 100;
+
+  FusionOffset offset;
+  FusionAhrs ahrs;
+
  public:
-  OrientationFilter(/* args */);
+  OrientationFilter(Vec3f acceleration, Vec3f angular_velocity, Vec3f magnetic_field);
   ~OrientationFilter();
+
+  Vec3f orientation;
 
   void initialize();
 
