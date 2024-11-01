ce87d8391eeec5abcde46abf08cdd57e5eaa024b
Krishna K Bharadwaj <ec22b1073@iiitdm.ac.in>
2024-11-01 17:35:54 +0530

    Update diagnostics_led.hpp

--- src/diagnostics_led/diagnostics_led.hpp
+++ src/diagnostics_led/diagnostics_led.hpp
@@ -1,17 +1,20 @@
-#ifndef SENSIING_AND_CONTROL_UNIT_SRC_DIAGNOSTICS_LED_DIAGNOSTICS_LED_HPP
-#define SENSIING_AND_CONTROL_UNIT_SRC_DIAGNOSTICS_LED_DIAGNOSTICS_LED_HPP
+#ifndef SENSING_AND_CONTROL_UNIT_SRC_DIAGNOSTICS_LED_DIAGNOSTICS_LED_HPP
+#define SENSING_AND_CONTROL_UNIT_SRC_DIAGNOSTICS_LED_DIAGNOSTICS_LED_HPP
+#include <Arduino.h>
+#include <stdint.h>
 
 namespace sensing_and_control_unit {
 class Diagnostics
 {
 private:
-    /* data */  
-
+    static const int16_t kLedCount = 9; 
+    const int16_t led_bitmask[kLedCount] = {1, 2, 4, 8, 16, 32, 64, 128, 256};
+    const int8_t led_pinmap[kLedCount] = {PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8};
 public:
-    Diagnostics(/* args */);
+    Diagnostics();
     ~Diagnostics();
-    void initialize();
-    void setLed(int led_indicator);
+    void initializeLED();
+    void setLed(int16_t led_indicator);
     
 };
 } // namespace sensing_and_control_unit
