20d9b5887e95aeb0829e5288175e2a45bb58447a
Krishna K Bharadwaj <ec22b1073@iiitdm.ac.in>
2024-11-03 00:21:07 +0530

    Update thruster.cpp

--- src/thruster/thruster.cpp
+++ src/thruster/thruster.cpp
@@ -2,11 +2,26 @@
 
 namespace sensing_and_control_unit {
 Thrusters::Thrusters() {}
-
 Thrusters::~Thrusters() {}
 
-void Thrusters::initialize() {}
+void Thrusters::initialize()
+{
+    for(int thruster_index=0;thruster_index<kThrusterCount;thruster_index++)
+    {
+        thrusters_[thruster_index].attach(kThrusterPins[thruster_index]);
+        thrusters_[thruster_index].writeMicroseconds(kThrusterNeutralPWM);
+    }
+}
 
-void Thrusters::setPWMs(int pwm_values[kThrusterCount]) {}
+void Thrusters::setPWMs(int pwm_values[kThrusterCount])
+{
+    int pwm_value;
+    for(int thruster_index=0;thruster_index<kThrusterCount;thruster_index++)
+    {
+        pwm_value = pwm_values[thruster_index];
+        thrusters_[thruster_index].writeMicroseconds(pwm_value);
+    }
+
+}
 
 }  // namespace sensing_and_control_unit
