027b5baa68f0c457dc118f2881a5c31711650123
techno113 <ec23b1132@iiitdm.ac.in>
2025-01-12 17:34:58 +0530

    updated depth sensor and mpu

--- src/depth_sensor/depth_sensor.cpp
+++ src/depth_sensor/depth_sensor.cpp
@@ -1,17 +1,46 @@
 #include "depth_sensor.hpp"
 
+MS5837 sensor;
+
 namespace sensing_and_control_unit {
-DepthSensor::DepthSensor() {}
-DepthSensor::~DepthSensor() {}
+DepthSensor::DepthSensor() {
 
-void DepthSensor::initialize() {}
+}
+DepthSensor::~DepthSensor() {
 
-void DepthSensor::calculateOffsets() {}
+}
 
-void DepthSensor::update() {}
+void DepthSensor::initialize() {
+    while(!sensor.init())
+    {
+        Serial.println("Initialization failed .");
+        delay(5000);
+    }
+    Serial.println("Initialization success");
+    calculateOffsets();
+}
 
-void DepthSensor::calibrate() {}
+void DepthSensor::calculateOffsets() {
+     for(int i=0;i<100;i++)
+     {
+        update();
+        depth_offset_+=depth_; 
+     }
+     depth_offset_/=100;
+}
 
-float DepthSensor::getDepth() { return 0; }
+void DepthSensor::update() {
+    sensor.read();
+    depth_=sensor.depth();
+}
+
+void DepthSensor::calibrate() {
+     depth_-=depth_offset_;
+}
+
+float DepthSensor::getDepth() { 
+    update();
+    calibrate();
+    return depth_ ; }
 
 }  // namespace sensing_and_control_unit
--- src/imu/imu.cpp
+++ src/imu/imu.cpp
@@ -4,18 +4,71 @@ namespace sensing_and_control_unit {
 Imu::Imu() {}
 Imu::~Imu() {}
 
-void Imu::initialize() {}
 
-void Imu::update() {}
+void Imu::initialize() {
+    while(!mpu.begin())
+    {
+        Serial.println(" MPU Connection failed");
+        delay(10);
+    }
+    Serial.println("MPU Connection successfull");
+     while(!mag.begin())
+    {
+        Serial.println(" FXOS8700 Connection failed");
+        delay(10);
+    }
+    Serial.println("FXOS8700 Connection successfull");
+    calculateOffsets();
+}
 
-void Imu::calculateOffsets() {}
 
-void Imu::calibrate() {}
+void Imu::update() {
+    sensors_event_t a,g,t,m ;
+    mpu.getEvent(&a, &g, &t);
+    mag.getEvent(&t ,&m);
+    acceleration_.x=a.acceleration.x;
+    acceleration_.y=a.acceleration.y;
+    acceleration_.z=a.acceleration.z;
+    angular_velocity_.x=g.gyro.x;
+    angular_velocity_.y=g.gyro.y;
+    angular_velocity_.z=g.gyro.z;
+    magnetic_field_.x=m.magnetic.x;
+    magnetic_field_.y=m.magnetic.y;
+    magnetic_field_.z=m.magnetic.z;
+}
 
-Vec3f Imu::getAcceleration() { return Vec3f(); }
+void Imu::calculateOffsets() {
+    for(int i=0;i<100;i++)
+    {
+        update();
+        accelerometer_offset_+= acceleration_;
+        gyroscope_offset_+= angular_velocity_;
+    }
+    accelerometer_offset_/=100;
+    gyroscope_offset_/=100;
+    accelerometer_offset_.z-=9.8;
+    }
 
-Vec3f Imu::getAngularVelocity() { return Vec3f(); }
 
-Vec3f Imu::getMagneticField() { return Vec3f(); }
+void Imu::calibrate() {
+     acceleration_-=accelerometer_offset_;
+     angular_velocity_-= gyroscope_offset_;
+    //  magnetic_field_= (magnetic_field-magnetometer_hard_iron_offset)*magnetometer_soft_iron_offset;
+}
 
-}  // namespace sensing_and_control_unit
+Vec3f Imu::getAcceleration() { 
+    update();
+    calibrate();
+    return acceleration_; }
+
+Vec3f Imu::getAngularVelocity() { 
+    update();
+    calibrate();
+    return angular_velocity_; }
+
+Vec3f Imu::getMagneticField() { 
+    update();
+    calibrate();
+    return magnetic_field_; }
+
+}
