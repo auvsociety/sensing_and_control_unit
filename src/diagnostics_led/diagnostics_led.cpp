3558f91d4badbb5f07d20ae227a32bc3cfbee744
krishna-k-b <krideep1111@gmail.com>
2025-01-13 12:31:02 +0530

    Update diagnostics_led.cpp

--- src/diagnostics_led/diagnostics_led.cpp
+++ src/diagnostics_led/diagnostics_led.cpp
@@ -2,11 +2,33 @@
 
 namespace sensing_and_control_unit {
 
-Diagnostics::Diagnostics(/* args */) {}
-
+Diagnostics::Diagnostics() {}
 Diagnostics::~Diagnostics() {}
 
-void Diagnostics::initialize() {}
+void Diagnostics::initializeLED() 
+{
+    for(int pin_index=0;pin_index<kLedCount;pin_index++)
+    {
+        pinMode(led_pinmap[pin_index],OUTPUT);
+        digitalWrite(led_pinmap[pin_index],HIGH);
+    }
+}
 
-void Diagnostics::setLed(int led_indicator) {}
+void Diagnostics::setLed(int16_t led_indicator) 
+{
+    for(int pin_index=0;pin_index<kLedCount;pin_index++)
+    {
+        if(led_indicator & led_bitmask[pin_index])
+        {
+            digitalWrite(led_pinmap[pin_index],LOW);
+        }
+        else
+        {
+            digitalWrite(led_pinmap[pin_index],HIGH);
+        }
+    }
+}
 }  // namespace sensing_and_control_unit
+
+
+
