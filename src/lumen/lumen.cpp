65c6493dd29f6df52ff2b627bdbd43458c836fc6
krishna-k-b <krideep1111@gmail.com>
2025-01-13 12:24:32 +0530

    Update lumen.cpp

--- src/lumen/lumen.cpp
+++ src/lumen/lumen.cpp
@@ -1,11 +1,24 @@
 #include "lumen.hpp"
 
 namespace sensing_and_control_unit {
+
 Lumen::Lumen() {}
 Lumen::~Lumen() {}
 
-void Lumen::initialize() {}
+void Lumen::initialize()
+{
+     lumen_.attach(kLumenPin);   
+    lumen_.writeMicroseconds(min_pwm);//off
+}
 
-void Lumen::setBrightness(int brightness) {}
+// void Lumen::initialize(ros::NodeHandle &nh) {
+//     lumen_.attach(kLumenPin);   
+//     lumen_.writeMicroseconds(min_pwm);//off
+// }
+
+void Lumen::setBrightness(int brightness) {
+    int pwm = map(brightness, 0, 100, min_pwm, max_pwm);   
+    lumen_.writeMicroseconds(pwm);   
+}
 
 }  // namespace sensing_and_control_unit
